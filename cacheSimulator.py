7a63c3ee1cdd25a649bd999eed342adad48bc9c7
Edditado <eshurtad@espol.edu.ec>
2015-09-06 13:44:05 -0500

    Mejoras en algoritmo optimo

--- cacheSimulator.py
+++ cacheSimulator.py
@@ -4,67 +4,44 @@ import sys
 
 #Funcion de Algoritmo Optimo
 def optimumAlg(lines, cacheSize):
-	#Diccionario de lineas con las posiciones donde se vuelven a repetir, 
-	#para facilitar busqueda de la linea menos usada en el futuro de la cache:
-	#{"linea":[pos1,pos2,..]}
 	wlDict = {}
 	wlSize = len(lines)	
 	for i in xrange(wlSize):
 		if lines[i] not in wlDict:
 			wlDict[lines[i]] = [i]
-
 		else:
 			wlDict[lines[i]].append(i)
 
-	#print len(wlDict)
+	for line in wlDict:
+		wlDict[line].append(sys.maxint)
 
-	#La cache será un Set (Conjunto) de lineas, los sets se forman de elementos unicos y su acceso es muy rápido
-	cache = set([])
-
-	#Numero de misses
+	cache = {}
+	futureItems = {}
 	misses = 0
-	#Iteracion de la tupla de lineas
+
 	for i in xrange(wlSize):
-		#print i
-
-		#Se remueve la posición de la linea actual de wlDict, así el primer elemento del arreglo 
-		#de dicha linea será la próxima posición en donde será usada nuevamente.
+		print i
 		wlDict[lines[i]].remove(i)
+		nextTime = wlDict[lines[i]][0]
 		if lines[i] not in cache:
+			misses += 1			
 			if len(cache) < cacheSize:
-				cache.add(lines[i])
-				misses += 1
-				#print cache
+				futureItems[nextTime] = lines[i]
+				cache[lines[i]] = nextTime
 			else:
-				#Diccionario de lineas futuras del cache: {pos:"linea",...}:
-				futureItems = {}
-				#Iteración de las lineas en cache
-				for item in cache:
-					#Si el arreglo (valor) de la linea actual(llave) en wlDict está vacio
-					#significa que la linea actual no se repetirá en el futuro
-					if wlDict[item] == []:
-						#Se reemplaza la linea actual del cache por la linea actual de la tupla de lineas
-						#y se sale del ciclo
-						cache.discard(item)
-						cache.add(lines[i])
-						futureItems = {}
-						misses += 1
-						#print cache
-						break
-					else:
-						#Se va añadiendo el primer elemento del arreglo(de wlDict) de la linea actual(de cache) 
-						#al diccionario de lineas futuras
-						futureItems[wlDict[item][0]] = item
-
-				if futureItems != {}:
-					#Se reemplaza el mayor del diccionario de lineas futuras, es decir el que será
-					#el menos usado en el futuro, por la linea actual de la tupla de lineas
-					cache.discard(futureItems[max(futureItems)])
-					cache.add(lines[i])
-					misses += 1
-					#print cache
+				maxItemPos = max(futureItems)
+				del cache[futureItems[maxItemPos]]
+				del futureItems[maxItemPos]
+				cache[lines[i]] = nextTime
+				futureItems[nextTime] = lines[i]
+		else:
+			del futureItems[cache[lines[i]]]
+			futureItems[nextTime] = lines[i]
+			cache[lines[i]] = nextTime
 
 	return misses
+
+
 
 '''Function LRU'''
 def lruAlg(lines,cacheSize):
@@ -173,7 +150,7 @@ def clockAlg(lines, cacheSize):
 
 #Obtencion de argumentos de la linea de comandos
 wlFile = sys.argv[1]
-policy = sys.argv[2]
+policy = sys.argv[2].upper()
 cacheSize = int(sys.argv[3])
 
 with open(wlFile,"r") as f:
@@ -184,15 +161,14 @@ lines = tuple(lines)
 print "\nEvaluando una caché",policy,"con",cacheSize,"entradas."
 print "Procesando...\n"
 
-if(policy == "OPTIMO" or policy == "ÓPTIMO" or policy == "optimo" or policy == "óptimo"):
-	
-	
+if(policy == "OPTIMO"):	
 	misses = optimumAlg(lines, cacheSize)
-if(policy == "LRU" or policy == "lru"):
+	
+if(policy == "LRU"):
 	misses = lruAlg(lines,cacheSize)
-	#misses = lruAlg(lines, cacheSize)
 
-if(policy == "clock" or policy == "Clock" or policy == "Clock"):
+if(policy == "CLOCK"):
 	misses = clockAlg(lines, cacheSize)
+	
 print "Resultados:"
 print "\tMiss rate: ",((float(misses)/len(lines))*100),"%  (",misses,"misses de",len(lines),"referencias ).\n"
